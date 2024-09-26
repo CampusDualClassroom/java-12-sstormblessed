7928b3f8418618f00007eff07b826d1b756bcc7f
Nicolás Vázquez <nikovazquez52@gmail.com>
2024-09-26 12:53:50 +0200

    ejercicio completo

--- src/main/java/com/campusdual/classroom/Exercise12.java
+++ src/main/java/com/campusdual/classroom/Exercise12.java
@@ -2,7 +2,27 @@ package com.campusdual.classroom;
 
 public class Exercise12 {
     public static void main(String[] args) {
+        Car coche = new Car();
 
+        coche.isTachometerEqualToZero();
+        coche.start();
+        coche.start();
+        coche.stop();
+        coche.stop();
+        coche.accelerate();
+        while (coche.speedometer != 0) {
+            coche.accelerate();
+        }
+        coche.brake();
+        while (coche.speedometer > 0) {
+            coche.brake();
+        }
+        coche.turnAngleOfWheels(20);
+        coche.turnAngleOfWheels(200);
+        coche.turnAngleOfWheels(-200);
+        coche.setReverse(true);
+        coche.accelerate();
+        coche.setReverse(true);
     }
 
 }
